44b77aa064fffaa8cbb9a247ebacf1a67c47a772
alfonso91 <alfonso91fp@gmail.com>
2023-01-23 18:29:27 +0100

    Directivas

--- src/app/ComponentesDirectivas/directivas/directivas.component.ts
+++ src/app/ComponentesDirectivas/directivas/directivas.component.ts
@@ -7,4 +7,102 @@ import { Component } from '@angular/core';
 })
 export class DirectivasComponent {
 
+
+/*EJEMPLO 1***********************/
+fondoAmarillo: boolean = false;
+letraGrande: boolean = false;
+
+checkFondo(){
+  this.fondoAmarillo=!this.fondoAmarillo;
+}
+
+checkLetra(){
+  this.letraGrande=!this.letraGrande;
+}
+
+asignaClases(){
+  let classes = {
+    fa: this.fondoAmarillo,
+    lg: this.letraGrande
+  };
+  
+  return classes;
+}
+/*********************************/
+
+
+/*EJEMPLO 2***********************/
+grupo1 = false;
+grupo2 = false;
+subgrupo11 = false;
+subgrupo12 = false;
+subgrupo21 = false;
+subgrupo22 = false;
+/*********************************/
+
+/*EJEMPLO 2***********************/
+//Array de objetos 
+public nombres = [
+  {nom:"uno",edad:10},
+  {nom:"dos",edad:20},
+  {nom:"tres",edad:30}
+];
+
+/*********************************/
+
+/*EJEMPLO 4***********************/
+opcion: string = '';
+
+/*********************************/
+
+/*EJEMPLO 5***********************/
+nombre: string = '';
+nombre2: string = '';
+
+cambiaNombre(evento: any) {
+  this.nombre=this.nombre.toLowerCase();
+  this.nombre2=this.nombre.toUpperCase();
+}
+
+cambiaNombre2(evento: any) {
+  this.nombre2=this.nombre2.toUpperCase();
+  this.nombre=this.nombre2.toLowerCase();
+}
+
+/*********************************/
+
+/*EJEMPLO 6***********************/
+color = 'blue';
+hayLetraGrande:boolean = false;
+hayColorFondo:boolean = false;
+hayLetraColor:boolean = false;
+hayLetraItalica:boolean = false;
+
+defineEstilos(){
+  let styles= {
+    'font-size': this.hayLetraGrande ? '300%' : 'initial',
+    'background-color': this.hayColorFondo ? '#58FA58' :
+    '#FFFFFF',
+    'color': this.hayLetraColor ? 'red' : 'black',
+    'font-style': this.hayLetraItalica ? 'italic' : 'normal'
+  };
+  return styles;
+}
+
+/*********************************/
+
+/*EJERCICIO FINAL***********************/
+mostrar = false;
+numeros:number [] = [1,2,3,4,5,6];
+pares:boolean = true; impares:boolean = true;
+colorPar:string= 'blue'; 
+colorImpar:string = 'red';
+
+clActividad(valor:any){
+  let classes = { activo: valor, inactivo: !valor };
+  return classes;
+}
+
+/*********************************/
+
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConceptosComponent } from './Componentes/conceptos/conceptos.component';
 import { IntroComponent } from './Componentes/intro/intro.component';
+import { DirectivasComponent } from './ComponentesDirectivas/directivas/directivas.component';
 
 const routes: Routes = [
   { path: '', component: IntroComponent },
   { path: 'conceptos', component: ConceptosComponent },
+  { path: 'directivas', component: DirectivasComponent },
 ];
 
 @NgModule({
